a9bd79bc5b208b31ab222f2047722abb5c97cdb2
dirtyb1rd <yeskov777.888@gmail.com>
2022-02-18 14:56:04 +0300

    added page catalog

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,11 @@ $(document).ready(function () {
     $('.header__burger').click(function (event) {
         $('.header__nav').toggleClass('active');
     });
+
+    $('.nav__close').click(function (event) {
+        $('.header__nav').removeClass('active');
+    })
 });
+
+const details = document.getElementById('sub-filter');
+details.open = true;
